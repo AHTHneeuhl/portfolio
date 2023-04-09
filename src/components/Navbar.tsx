65c82e62bfd802bdd06438a4da43061253e6ecbb
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-04-09 19:25:58 +0530

    Try to add Navbar and Hero (#1)

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar: React.FC = () => {
+  return (
+    <div className="py-4 bg-slate-100 shadow-md">
+      <div className="m-auto max-w-[84%]">
+        <h3 className="text-blue-950 font-semibold text-xl cursor-pointer">
+          Shubham Patrick
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
