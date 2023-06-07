e25bde46d5e9a1b92d5e2df2295044b2ff3747cf
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-07 13:06:24 +0530

    feat-styles: Update skills component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,13 +2,24 @@
 
 import sidebar from "@/config/sidebar";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidebar: React.FC = () => {
+  const pathname = usePathname();
+
   return (
-    <div className="fixed top-0 left-0 flex flex-col items-start justify-center gap-2 p-4 h-screen">
+    <div className="fixed top-0 left-0 w-48 flex flex-col items-start justify-center gap-3 p-4 h-screen">
       {sidebar.map(({ title, path }) => (
         <Link key={title} href={path}>
-          <span className="text-neutral-700 font-normal">{title}</span>
+          <span
+            className={`${
+              pathname === path
+                ? "text-blue-700 font-semibold bg-blue-100 rounded-md"
+                : "font-medium text-neutral-500"
+            } p-1 w-full`}
+          >
+            {title}
+          </span>
         </Link>
       ))}
     </div>
