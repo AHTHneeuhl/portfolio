ecca5750f5a8a59959fac2cad79aba520a8acabe
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-06-07 13:35:46 +0530

    feat-styles: Update experiences component (#13)

--- src/app/experiences/Organization.tsx
+++ src/app/experiences/Organization.tsx
@@ -4,6 +4,7 @@ type TProps = {
   title: string;
   company: string;
   location: string;
+  works: string[];
   period: string;
   skills: string[];
 };
@@ -12,26 +13,36 @@ const Organization: React.FC<TProps> = ({
   title,
   company,
   location,
+  works,
   period,
   skills,
 }) => {
   return (
-    <div className="w-[720px]">
+    <div className="cursor-pointer border border-white hover:border-neutral-200 p-4 rounded-xl">
       <h4 className="text-lg font-medium text-neutral-800">
         {title} <span className="text-neutral-500 text-sm">at</span> {company}{" "}
       </h4>
       <div className="flex flex-row justify-between items-center">
         <p className="italic text-neutral-500 text-xs">{location}</p>
-        <p className="text-teal-700 text-xs italic">{period}</p>
+        <p className="text-teal-700 text-xs font-medium italic">{period}</p>
       </div>
-
-      <ul className="flex flex-row flex-wrap items-center gap-2">
+      <ul className="flex flex-row flex-wrap items-center gap-2 my-2">
         {skills.map((skill) => (
           <li
             key={skill}
-            className="text-neutral-700 text-xs leading-6 bg-neutral-100 px-1 italic rounded-md"
+            className="text-purple-800 text-xs leading-6 bg-purple-50 px-1 italic rounded-md"
           >
             {skill}
+          </li>
+        ))}
+      </ul>
+      <ul className="flex flex-col items-start gap-1 my-2">
+        {works.map((work) => (
+          <li
+            key={work}
+            className="text-neutral-600 font-medium text-xs leading-6"
+          >
+            {work}
           </li>
         ))}
       </ul>
