956ba7e99e4a6ba8d6e11463f9d776b88ced530a
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-08-01 12:30:13 +0530

    feat: add animation in work cards (#22)

--- src/components/works.tsx
+++ src/components/works.tsx
@@ -1,21 +1,25 @@
 "use client";
 
+import { AnimatePresence } from "framer-motion";
 import { Card } from "@/components";
 import works from "@/config/works";
 
 const Works: React.FC = () => {
   return (
-    <div className="py-8 px-6 flex flex-col gap-6">
-      {works.map(({ name, description, source, techStack }) => (
-        <Card
-          key={name}
-          title={name}
-          description={description}
-          link={source}
-          techStack={techStack}
-        />
-      ))}
-    </div>
+    <AnimatePresence>
+      <div className="py-8 px-6 flex flex-col gap-6">
+        {works.map(({ name, description, source, techStack }, index) => (
+          <Card
+            key={name}
+            title={name}
+            description={description}
+            link={source}
+            techStack={techStack}
+            index={index}
+          />
+        ))}
+      </div>
+    </AnimatePresence>
   );
 };
 
